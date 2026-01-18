4dc282194b7f419042790be1653ad3a5ee554696
JumpyJay <iuswtric1909@gmail.com>
2026-01-18 20:32:19 +0800

    Add routing

---
+++ src/pages/JaysPage.tsx
@@ -0,0 +1,8 @@
+export default function JaysPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-5xl font-bold">Jay's Page</h1>
+      <p className="text-2xl">Jay is a v5 boulderer.</p>
+    </div>
+  );
+}
